1ea1fcb98442dd1c0438fb03cfd27b81e2b823b4
Zokky2e <icro4.gaming@hotmail.com>
2025-02-14 16:28:25 +0100

    sorting files and adding damage invunerability

--- Assets/Scripts/Health/Health.cs
+++ Assets/Scripts/Health/Health.cs
@@ -1,16 +1,32 @@
 using Assets.Scripts;
+using System;
+using System.Collections;
 using UnityEngine;
 
 public class Health : MonoBehaviour
 {
+    [Header ("Health")]
     public float startingHealth;
     public float currentHealth { get; private set; }
 
     public IEntity entity;
 
+    [Header("IFrames")]
+    public float iFramesDuration;
+    public int numberOffFlashes;
+    private SpriteRenderer spriteRend;
+    [SerializeField] private LayerMask playerLayer;
+    [SerializeField] private LayerMask enemyLayer;
+
+    private int playerLayerNumber;
+    private int enemyLayerNumber;
+
     public void Awake()
     {
         currentHealth = startingHealth;
+        spriteRend = GetComponent<SpriteRenderer>();
+        playerLayerNumber = (int)Math.Log(playerLayer.value, 2);
+        enemyLayerNumber = (int)Math.Log(enemyLayer.value, 2);
     }
 
     public void TakeDamage(float _damage)
@@ -19,6 +35,7 @@ public class Health : MonoBehaviour
         if (currentHealth > 0)
         {
             entity.TakeDamage();
+            StartCoroutine(Invunerability());
         }
         else
         {
@@ -29,6 +46,25 @@ public class Health : MonoBehaviour
     public void AddHealth(float _healthAmount)
     {
         currentHealth = Mathf.Clamp(currentHealth + _healthAmount, 0, startingHealth);
+    }
+
+    private IEnumerator Invunerability()
+    {
+        Debug.Log(playerLayerNumber);
+        if (playerLayerNumber == 6)
+        {
+            Physics2D.IgnoreLayerCollision(playerLayerNumber, enemyLayerNumber, true);
+            for (int i = 0; i < numberOffFlashes; i++) 
+            {
+                spriteRend.color = new Color(1, 0, 0, 0.9f);
+                yield return new WaitForSeconds(iFramesDuration / (numberOffFlashes * 2));
+                spriteRend.color = Color.white;
+                yield return new WaitForSeconds(iFramesDuration / (numberOffFlashes * 2));
+            }
+            Physics2D.IgnoreLayerCollision(playerLayerNumber, enemyLayerNumber, false);
+        }
+
+        yield break;
     }
 
 }
