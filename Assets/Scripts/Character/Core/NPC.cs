b551e28cfc37eaddd03007e06deb9ec01f7256d3
Zokky2e <icro4.gaming@hotmail.com>
2025-02-18 02:01:25 +0100

    adding simple merchant pixel art and npc scripting

---
+++ Assets/Scripts/Character/Core/NPC.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    public bool facingToRight = true;
+    private SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>(); // Get the SpriteRenderer component
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Debug.Log("testing trigger");
+        Debug.Log(collision.tag);
+        if (collision.tag == "Player")
+        {
+            Transform player = collision.transform;
+
+            if (player.position.x < transform.position.x && facingToRight)
+            {
+                Flip();
+            }
+            else if (player.position.x > transform.position.x && !facingToRight)
+            {
+                Flip();
+            }
+        }
+    }
+    private void Flip()
+    {
+        facingToRight = !facingToRight;
+        spriteRenderer.flipX = !spriteRenderer.flipX;
+    }
+}
