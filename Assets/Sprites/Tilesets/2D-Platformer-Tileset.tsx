ead2eabf22f60d9d0df558eb79f8855c4c2010ac
Zokky2e <icro4.gaming@hotmail.com>
2025-02-19 17:34:08 +0100

    creating tilesets and rules for tilesets for new level designs

---
+++ Assets/Sprites/Tilesets/2D-Platformer-Tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="2D-Platformer-Tileset" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="../../Cainos/Pixel Art Platformer - Village Props/Texture/TX Tileset Ground.png" trans="000000" width="512" height="512"/>
+</tileset>
