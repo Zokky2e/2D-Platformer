8f8f3c901e8f637f5289d819662c6d72c8bffa98
Zokky2e <icro4.gaming@hotmail.com>
2025-03-04 01:59:15 +0100

    creating healthbar separators

--- Assets/Scripts/Health/Healthbar.cs
+++ Assets/Scripts/Health/Healthbar.cs
@@ -1,15 +1,24 @@
 using UnityEngine;
 using TMPro;
+using System.Collections.Generic;
+using System.Dynamic;
+using System;
+using Unity.VisualScripting;
 
 public class Healthbar : MonoBehaviour
 {
     public Health entityHealth;
     public UnityEngine.UI.Slider healthValue;
+    public RectTransform healthBarFill;
+    public GameObject breakpointPrefab;
+    public int breakpointEveryX = 25;
     public TextMeshProUGUI healthText;
+    public List<GameObject> markers = new List<GameObject>();
 
     public void Start()
     {
         setHealthbar();
+        createBreakpoints();
     }
 
     public void Update()
@@ -26,5 +35,30 @@ public class Healthbar : MonoBehaviour
             healthText.text = PersistentPlayerHealth.Instance.currentHealth.ToString();
             healthValue.value = healthPercent;
         }
+    }
+
+    private void createBreakpoints()
+    {
+
+        if (PersistentPlayerHealth.Instance != null)
+        {
+            float maxHealth = PersistentPlayerHealth.Instance.startingHealth;
+            float currentHealth = PersistentPlayerHealth.Instance.currentHealth;
+            int currentBreakpoint = breakpointEveryX;
+            while (currentBreakpoint < currentHealth)
+            {
+                float normalizedPos = (float)currentBreakpoint / maxHealth;
+                CreateBreakpoint(normalizedPos);
+                currentBreakpoint += breakpointEveryX;
+            }
+        }
+    }
+
+    private void CreateBreakpoint(float normalizedPos)
+    {
+
+        GameObject marker = Instantiate(breakpointPrefab, healthBarFill);
+        marker.transform.localPosition = new Vector3(normalizedPos * healthBarFill.rect.width, 0, 0);
+        markers.Add(marker);
     }
 }
