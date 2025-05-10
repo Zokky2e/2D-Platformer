eb931fc174400b8c6b1bc1d3adea7b84c463fdc9
Zokky2e <icro4.gaming@hotmail.com>
2025-05-10 22:53:58 +0200

    implementing loot logic

---
+++ Assets/Scripts/Items/LootSystem/LootChest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LootChest : MonoBehaviour
+{
+    private Interactable interactable; // Reference to interactable component
+
+    public LootInventory LootInventory;
+
+    public List<Item> Loot;
+
+    private void Start()
+    {
+        interactable = gameObject.AddComponent<Interactable>();
+        interactable.onInteract = OpenChest; // Assign interaction behavior
+        Loot = LootInventory.GetLoot();
+    }
+
+
+    private void OpenChest()
+    {
+    }
+}
---
+++ Assets/Scripts/Items/LootSystem/LootInventory.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[System.Serializable]
+public class LootItemData
+{
+    public List<int> itemIds;
+    public int showChance;
+}
+
+[CreateAssetMenu(menuName = "Loot/LootInventory")]
+public class LootInventory : ScriptableObject
+{
+    public List<LootItemData> itemsData;
+
+    public List<Item> GetLoot()
+    {
+        List<Item> items = new List<Item>();
+        float totalChance = 0f;
+        foreach (var loot in itemsData)
+        {
+            totalChance += loot.showChance;
+        }
+
+        float randomValue = Random.Range(0f, totalChance);
+        float currentChance = 0f;
+
+        foreach (var loot in itemsData)
+        {
+            currentChance += loot.showChance;
+            if (randomValue <= currentChance)
+            {
+                items = ItemDatabase.Instance.GetItemsByIds(loot.itemIds.ToArray());
+            }
+        }
+        return items;
+    }
+}
